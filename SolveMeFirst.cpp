a4fe9e9514b60a4ee3cca2a231ce2d98fc454793
revan wirawan <revanwirawan36@gmail.com>
2025-11-27 23:19:50 +0700

    Create SolveMeFirst.cpp

---
+++ SolveMeFirst.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int hasil(int a, int b){
+    int jumlah=a+b;
+    return jumlah;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout << hasil(a,b);
+}
