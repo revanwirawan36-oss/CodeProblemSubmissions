f5476146552a9a013e272069825ceb519ba081fd
revan wirawan <revanwirawan36@gmail.com>
2026-01-09 13:34:31 +0700

    Update FlatlandSpaceStations.py

--- hr/FlatlandSpaceStations.py
+++ hr/FlatlandSpaceStations.py
@@ -1,28 +1,10 @@
 import math
 def flatland_ss(cities,n, stations):
-    maxDis=-1
     stations.sort()
-    #print(stations)
-    for i in range(len(stations)-1):
-        dis=abs(stations[i]-stations[i+1])
-        maxDis=max(dis,maxDis)
-    
-    maxTemp=maxDis
-    maxDis=max(abs(stations[0]-0), maxDis//2)
-    
-    if maxTemp==maxDis:
-        maxDis=max(abs(stations[n-1]-(cities-1)), maxDis//2)
-    else:
-        maxDis=max(abs(stations[n-1]-(cities-1)), maxDis)
-    
-    if maxTemp==maxDis:
-        return maxDis//2
+    maxDis=max(abs(stations[0]-0), abs(stations[n-1]-(cities-1)))
+    for i in range(n-1):
+        maxDis=max(abs(stations[i]-stations[i+1])//2, maxDis)
     return maxDis
-    
-    '''
-    maxGakeganti=maxTemp//2
-    print(f"keganti: {maxKeganti}, gak: {maxGakeganti}")
-    '''
     
 if __name__ == '__main__':
     cities,n=map(int, input().split())
