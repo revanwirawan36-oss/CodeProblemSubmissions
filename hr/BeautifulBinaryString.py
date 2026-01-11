8c2d76306f554533f547fb26c6d6316744bd67fc
revan wirawan <revanwirawan36@gmail.com>
2026-01-11 18:49:31 +0700

    Create BeautifulBinaryString.py

---
+++ hr/BeautifulBinaryString.py
@@ -0,0 +1,16 @@
+def beautiful_binary_string(n,b):
+    ada=0
+    i=0
+    while i < n-2:
+        if b[i:i+3]=="010":
+            ada+=1a
+            i+=3
+        else:
+            i+=1
+    return ada 
+    
+if __name__=='__main__':
+    n=int(input())
+    b=input()
+    
+    print(beautiful_binary_string(n,b))
