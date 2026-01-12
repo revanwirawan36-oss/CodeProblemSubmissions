15ce5ecce14158ec515dd20fb48df7c5cedced4e
revan wirawan <revanwirawan36@gmail.com>
2026-01-12 16:02:37 +0700

    Update CavityMap.py

--- hr/CavityMap.py
+++ hr/CavityMap.py
@@ -2,7 +2,7 @@ def cavity_map(N, mat):
     n=len(mat)
     print(mat[0])
     if n>2:
-        for i in range(1, n-1):
+        for i in range(1, n-1): #hm
             liste=list(mat[i])
             for j in range(1,len(liste)-1):
                 if (liste[j-1]!='X' and int(liste[j])>int(liste[j-1])) and (liste[j+1]!='X' and int(liste[j])>int(liste[j+1])) and (mat[i-1][j]!='X' and int(liste[j])>int(mat[i-1][j])) and ( mat[i+1][j]!='X' and int(liste[j])>int(mat[i+1][j])):
