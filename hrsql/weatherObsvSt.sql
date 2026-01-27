1aa130b4c01855ba3288fa7612cd3db86e15ad46
revan wirawan <revanwirawan36@gmail.com>
2026-01-27 19:11:07 +0700

    Create weatherObsvSt.sql

---
+++ hrsql/weatherObsvSt.sql
@@ -0,0 +1,12 @@
+/*
+Enter your query here.
+*/
+select city, length(city) 
+from station 
+order by length(city), 
+city limit 1;
+
+select city, length(city) 
+from station 
+order by length(city) desc, 
+city limit 1;
