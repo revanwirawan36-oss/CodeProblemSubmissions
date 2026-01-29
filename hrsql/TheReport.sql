9b6126a0d42dfbf73aa7245f3800742be1732f5a
revan wirawan <revanwirawan36@gmail.com>
2026-01-29 20:26:42 +0700

    Create TheReport.sql

---
+++ hrsql/TheReport.sql
@@ -0,0 +1,13 @@
+select(
+case
+    when g.grade<8 then "NULL"
+    else s.name
+    end)
+    as name, g.grade, s.marks
+from grades g 
+cross join students s 
+where s.marks between g.min_mark and g.max_mark 
+order by g.grade desc, 
+case when g.grade between 8 and 10 then s.name end asc,
+case when g.grade between 1 and 7 then s.marks end asc;
+
