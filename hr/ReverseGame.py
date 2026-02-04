6a045a31e6e4fa6a227aec34b41df9040e8798cb
revan wirawan <revanwirawan36@gmail.com>
2026-02-04 12:42:19 +0700

    Refactor reverse_game function implementation
    Refactor reverse_game function to simplify logic and improve readability.

--- hr/ReverseGame.py
+++ hr/ReverseGame.py
@@ -1,22 +1,9 @@
 def reverse_game(N,K):
-    cpyN=N-1
-    hasil=N-1
+    N-=1
     
-    if K==N-1:
-        return 0
-        
-    for i in range(1, N):
-        if i%2==0:
-            hasil+=cpyN
-          #  print(f"hasilnya segini: {hasil}, copyN= {cpyN}")
-        else:
-            hasil-=cpyN
-           # print(f"hasilnya segini: {hasil}, copyN= {cpyN}")
-        
-        if hasil==K:
-            break
-        cpyN-=1
-    return i
+    if K<N//2:
+        return (K*2)+1
+    return (N-K)*2
     
 if __name__ =='__main__':
     T=int(input())
