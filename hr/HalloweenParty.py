c79676dc44ba39e958e5b61ab6badcef6cd7ae2d
revan wirawan <revanwirawan36@gmail.com>
2026-02-05 17:47:06 +0700

    Add Halloween party function and input handling

---
+++ hr/HalloweenParty.py
@@ -0,0 +1,9 @@
+def halloween_party(K):
+    return (K//2)*(K-(K//2))
+    
+if __name__=='__main__':
+    T=int(input())
+    
+    for _ in range(T):
+        K=int(input())
+        print(halloween_party(K))
