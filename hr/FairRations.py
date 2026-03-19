ed35e16e584dc34b4dc895a8197347dbd9d2cb05
revan wirawan <revanwirawan36@gmail.com>
2026-03-19 18:11:57 +0700

    Create FairRations.py

---
+++ hr/FairRations.py
@@ -0,0 +1,18 @@
+def fairRations(B):
+    kasih=0
+    for i in range(len(B)-1):
+        if B[i]%2!=0:
+            B[i+1]+=1
+            B[i]+=1
+            kasih+=2
+        #print(B)
+    for el in B:
+        if el%2!=0:
+            return "NO"
+    return kasih
+
+if __name__ == '__main__':
+    n=int(input())
+    B=list(map(int, input().split()))
+    
+    print(fairRations(B))
