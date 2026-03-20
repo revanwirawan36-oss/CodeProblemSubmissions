008ae9b660d8526793d4020f893db6bcba557c39
revan wirawan <revanwirawan36@gmail.com>
2026-03-20 22:20:00 +0700

    Update FairRations.py

--- hr/FairRations.py
+++ hr/FairRations.py
@@ -8,7 +8,7 @@ def fairRations(B):
         #print(B)
     for el in B:
         if el%2!=0:
-            return "NO"
+            return "NO" #krna ganjil
     return kasih
 
 if __name__ == '__main__':
